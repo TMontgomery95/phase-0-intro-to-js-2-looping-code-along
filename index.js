c2eeb806a2fc63358834d9766f774a0b3e95e1f0
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-11 13:36:26 -0400

    commit tree d3188fb626c8a72d2d3730ef6fe8eb458d640c5a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names, event) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event}` + ` gift!`)
+  }
+  return messages;
+}
+
+console.log(writeCards(names, "event"));
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+    console.log(countdown--);
+    }
+}
+  
+  countDown(10);
